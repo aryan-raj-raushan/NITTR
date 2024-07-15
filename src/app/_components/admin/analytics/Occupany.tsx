ca17acc9aabb724ec06585fed7596d0105d33078
aryan-raj-raushan <aryanraj.raushan@gmail.com>
2024-07-15 07:24:09 +0530

    Occupancy report -for booking

--- src/app/_components/admin/analytics/Occupany.tsx
+++ src/app/_components/admin/analytics/Occupany.tsx
@@ -1,10 +1,23 @@
 "use client";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import {
-  BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer
-} from 'recharts';
-import { format, startOfWeek, startOfMonth } from 'date-fns';
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import {
+  format,
+  eachDayOfInterval,
+  addMonths,
+  startOfWeek,
+  startOfMonth,
+} from "date-fns";
 
 import { RouterOutputs } from "~/trpc/shared";
 import AdminNav from "../AdminNav";
@@ -12,50 +25,130 @@ import { IoChevronDownOutline } from "react-icons/io5";
 
 export default function OccupancyReport({
   bookings,
+  roomDetails,
 }: {
   bookings: RouterOutputs["booking"]["getAllBookings"]["bookings"];
+  roomDetails: any[]; // Replace with the actual type of roomDetails
 }) {
-  const [timeframe, setTimeframe] = useState<'daily' | 'weekly' | 'monthly'>('daily');
+  const [hostel, setHostel] = useState("saran");
+  const [timeframe, setTimeframe] = useState<
+    "daily" | "weekly" | "monthly" | "custom"
+  >("daily");
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  // Data transformation based on the selected timeframe
-  const data = useMemo(() => {
+  const hostels = useMemo(() => {
+    // Extract unique hostel names from roomDetails
+    const uniqueHostels = Array.from(
+      new Set(roomDetails.map((room) => room.hostelName)),
+    );
+    return uniqueHostels;
+  }, [roomDetails]);
+
+  const bookingData = useMemo(() => {
     let groupedData = {} as Record<string, number>;
 
-    if (timeframe === 'daily') {
-      groupedData = bookings.reduce((acc:any, booking:any) => {
-        const date = format(new Date(booking.bookingDate), 'dd-MM-yyyy');
-        if (!acc[date]) {
-          acc[date] = 0;
-        }
-        acc[date]++;
-        return acc;
-      }, {} as Record<string, number>);
-    } else if (timeframe === 'weekly') {
-      groupedData = bookings.reduce((acc:any, booking:any) => {
-        const weekStart = format(startOfWeek(new Date(booking.bookingDate)), 'dd-MM-yyyy');
-        if (!acc[weekStart]) {
-          acc[weekStart] = 0;
-        }
-        acc[weekStart]++;
-        return acc;
-      }, {} as Record<string, number>);
-    } else if (timeframe === 'monthly') {
-      groupedData = bookings.reduce((acc:any, booking:any) => {
-        const monthStart = format(new Date(booking.bookingDate), 'MMMM yyyy');
-        if (!acc[monthStart]) {
-          acc[monthStart] = 0;
-        }
-        acc[monthStart]++;
-        return acc;
-      }, {} as Record<string, number>);
+    if (timeframe === "daily") {
+      groupedData = bookings.reduce(
+        (acc: any, booking: any) => {
+          const date = format(new Date(booking.bookingDate), "dd-MM-yyyy");
+          if (!acc[date]) {
+            acc[date] = 0;
+          }
+          acc[date]++;
+          return acc;
+        },
+        {} as Record<string, number>,
+      );
+    } else if (timeframe === "weekly") {
+      groupedData = bookings.reduce(
+        (acc: any, booking: any) => {
+          const weekStart = format(
+            startOfWeek(new Date(booking.bookingDate)),
+            "dd-MM-yyyy",
+          );
+          if (!acc[weekStart]) {
+            acc[weekStart] = 0;
+          }
+          acc[weekStart]++;
+          return acc;
+        },
+        {} as Record<string, number>,
+      );
+    } else if (timeframe === "monthly") {
+      groupedData = bookings.reduce(
+        (acc: any, booking: any) => {
+          const monthStart = format(
+            startOfMonth(new Date(booking.bookingDate)),
+            "MMMM yyyy",
+          );
+          if (!acc[monthStart]) {
+            acc[monthStart] = 0;
+          }
+          acc[monthStart]++;
+          return acc;
+        },
+        {} as Record<string, number>,
+      );
     }
 
-    return Object.keys(groupedData).map(date => ({
+    return Object.keys(groupedData).map((date) => ({
       date,
       bookings: groupedData[date],
     }));
   }, [bookings, timeframe]);
+
+  // Data transformation for bed availability
+  const bedAvailabilityData = useMemo(() => {
+    const now = new Date();
+    const futureBookings = bookings.filter(
+      (booking: any) =>
+        new Date(booking.bookedFromDt) >= now &&
+        booking.bookingStatus === "CONFIRMED",
+    );
+
+    let groupedData: any = {} as Record<string, Record<string, number>>;
+
+    const filteredRoomDetails = roomDetails.filter(
+      (room) => room.hostelName === hostel,
+    );
+
+    const dates = eachDayOfInterval({ start: now, end: addMonths(now, 1) });
+    dates.forEach((date) => {
+      const formattedDate = format(date, "dd-MM-yyyy");
+      if (!groupedData[formattedDate]) {
+        groupedData[formattedDate] = {};
+      }
+
+      filteredRoomDetails.forEach((room) => {
+        const roomType = room.roomType;
+        if (!groupedData[formattedDate][roomType]) {
+          groupedData[formattedDate][roomType] = room.totalBed; // Total beds
+        }
+
+        futureBookings.forEach((booking: any) => {
+          if (
+            format(new Date(booking.bookedFromDt), "dd-MM-yyyy") ===
+              formattedDate &&
+            booking.roomType === roomType &&
+            booking.hostelName === hostel
+          ) {
+            groupedData[formattedDate][roomType] -= booking.bookedBed; // Subtract booked beds
+          }
+        });
+      });
+    });
+
+    let transformedData = [];
+    for (const date in groupedData) {
+      const dateData: any = { date };
+      for (const roomType in groupedData[date]) {
+        dateData[roomType] = groupedData[date][roomType];
+      }
+      transformedData.push(dateData);
+    }
+
+    return transformedData;
+  }, [bookings, timeframe, roomDetails, hostel]);
 
   return (
     <TooltipProvider>
@@ -65,46 +158,102 @@ export default function OccupancyReport({
             <AdminNav />
             <div className="w-full">
               <div className="flex items-center justify-between">
-              <div className="px-10 text-3xl font-medium pt-5 pb-10">
-                Occupancy Report
-              </div>
-              <div className="px-10 pb-5 relative">
-                <label htmlFor="timeframe" className="mr-3">Select Timeframe: </label>
-                <div className="relative inline-block">
-                  <select
-                    id="timeframe"
-                    value={timeframe}
-                    onChange={(e) => {
-                      setTimeframe(e.target.value as 'daily' | 'weekly' | 'monthly');
-                      setIsDropdownOpen(false);
-                    }}
-                    onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-                    className="px-4 py-1 w-28 cursor-pointer border border-gray-300 rounded appearance-none outline-none"
-                    style={{ WebkitAppearance: "none", MozAppearance: "none", appearance: "none" }}
-                  >
-                    <option value="daily">Daily</option>
-                    <option value="weekly">Weekly</option>
-                    <option value="monthly">Monthly</option>
-                  </select>
-                  <span
-                    className={`absolute right-3 top-2.5 duration-200 transition-transform ${
-                      isDropdownOpen ? 'rotate-180' : 'rotate-0'
-                    }`}
-                  >
-                    <IoChevronDownOutline/>
-                  </span>
+                <div className="px-10 pb-10 pt-5 text-3xl font-medium">
+                  Occupancy Report
+                </div>
+                <div className="relative px-10 pb-5">
+                  <label htmlFor="hostel" className="mr-3">
+                    Select Hostel:{" "}
+                  </label>
+                  <div className="relative inline-block">
+                    <select
+                      id="hostel"
+                      value={hostel}
+                      onChange={(e) => {
+                        setHostel(e.target.value);
+                      }}
+                      className="w-28 cursor-pointer appearance-none rounded border border-gray-300 px-4 py-1 outline-none"
+                      style={{
+                        WebkitAppearance: "none",
+                        MozAppearance: "none",
+                        appearance: "none",
+                      }}
+                    >
+                      {hostels.map((h) => (
+                        <option key={h} value={h}>
+                          {h}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <label htmlFor="timeframe" className="ml-5 mr-3">
+                    Select Timeframe:{" "}
+                  </label>
+                  <div className="relative inline-block">
+                    <select
+                      id="timeframe"
+                      value={timeframe}
+                      onChange={(e) => {
+                        setTimeframe(
+                          e.target.value as
+                            | "daily"
+                            | "weekly"
+                            | "monthly"
+                            | "custom",
+                        );
+                        setIsDropdownOpen(false);
+                      }}
+                      onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+                      className="w-28 cursor-pointer appearance-none rounded border border-gray-300 px-4 py-1 outline-none"
+                      style={{
+                        WebkitAppearance: "none",
+                        MozAppearance: "none",
+                        appearance: "none",
+                      }}
+                    >
+                      <option value="daily">Daily</option>
+                      <option value="weekly">Weekly</option>
+                      <option value="monthly">Monthly</option>
+                      <option value="custom">Custom</option>
+                    </select>
+                    <span
+                      className={`absolute right-3 top-2.5 transition-transform duration-200 ${
+                        isDropdownOpen ? "rotate-180" : "rotate-0"
+                      }`}
+                    >
+                      <IoChevronDownOutline />
+                    </span>
+                  </div>
                 </div>
               </div>
+
+              <div className="px-10 pb-10 pt-5 text-2xl font-medium">
+                Overall Booking Report
               </div>
-              
               <ResponsiveContainer width="100%" height={400}>
-                <BarChart data={data}>
+                <BarChart data={bookingData}>
                   <CartesianGrid strokeDasharray="3 3" />
                   <XAxis dataKey="date" />
                   <YAxis />
-                  <Tooltip  />
+                  <Tooltip />
                   <Legend />
                   <Bar dataKey="bookings" fill="#8884d8" />
+                </BarChart>
+              </ResponsiveContainer>
+
+              <div className="px-10 pb-10 pt-5 text-2xl font-medium">
+                Bed Availability Report
+              </div>
+              <ResponsiveContainer width="100%" height={400}>
+                <BarChart data={bedAvailabilityData}>
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="date" />
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <Bar dataKey="SINGLE_BED" fill="#82ca9d" />
+                  <Bar dataKey="DOUBLE_BED" fill="#8884d8" />
+                  <Bar dataKey="TRIPLE_BED" fill="#ffc658" />
                 </BarChart>
               </ResponsiveContainer>
             </div>
--- src/app/admin/occupancy-report/page.tsx
+++ src/app/admin/occupancy-report/page.tsx
@@ -4,12 +4,13 @@ import OccupancyReport from "~/app/_components/admin/analytics/Occupany";
 
 export default async function Page() {
   const { bookings } = await api.booking.getAllBookings.query({});
+  const { roomDetails } = await api.room.getAllRooms.mutate();
   if (bookings) {
     return (
       <OccupancyReport
-        // month={month}
         key={Math.random()}
         bookings={bookings}
+        roomDetails={roomDetails}
       ></OccupancyReport>
     );
   } else {
