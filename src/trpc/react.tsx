e564ca3945b36c3892d7fddd7d63c73bd489d7d8
saurabh <saurabhpanday21212@gmail.com>
2024-08-14 10:58:28 +0530

    payment capture

--- src/trpc/react.tsx
+++ src/trpc/react.tsx
@@ -4,16 +4,15 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client";
 import { createTRPCReact } from "@trpc/react-query";
 import { useState } from "react";
+import { SessionProvider } from "next-auth/react";
 
 import { type AppRouter } from "~/server/api/root";
 import { getUrl, transformer } from "./shared";
-import { SessionProvider } from "next-auth/react";
 
 export const api = createTRPCReact<AppRouter>();
 
-export function Providers(props: { children: React.ReactNode }) {
+export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
-
   const [trpcClient] = useState(() =>
     api.createClient({
       transformer,
@@ -31,14 +30,12 @@ export function Providers(props: { children: React.ReactNode }) {
   );
 
   return (
-
     <SessionProvider>
       <QueryClientProvider client={queryClient}>
         <api.Provider client={trpcClient} queryClient={queryClient}>
-          {props.children}
+          {children}
         </api.Provider>
       </QueryClientProvider>
     </SessionProvider>
-
   );
 }
