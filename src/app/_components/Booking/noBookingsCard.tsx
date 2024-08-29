b143f7208c341d799be572edf61286678ff86117
saurabhp0007 <saurabhpanday21212@gmail.com>
2024-08-29 20:08:19 +0530

    chnages

--- src/app/_components/Booking/noBookingsCard.tsx
+++ src/app/_components/Booking/noBookingsCard.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function NoBookingsCard() {
   return <Card className="p-10 grid grid-cols-4 justify-center items-center">
     <div className="flex justify-center items-center"></div>
-    <div className="col-span-2 space-y-2">
+    <div className="col-span-2 space-y-2 min-h-[50vh]">
       <div className="font-extrabold">Looks Empty , You've no Bookings</div>
       <div className="text-sm text-gray-500">Would you like to make a booking ?</div>
       <Button><Link href='/'>Make a Booking</Link></Button>
